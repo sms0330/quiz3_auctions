3265901c4b5e81481a9831b2880aac69cc9a79a5
Joseph Son <sms0330@gmail.com>
2021-08-24 11:34:06 -0700

    Added API serializer settings

---
+++ biddr_api/app/serializers/auction_serializer.rb
@@ -0,0 +1,12 @@
+class AuctionSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :ends_at, :reserve_price, :created_at
+
+  belongs_to :user, key: :owner
+  has_many :bids
+
+  class BidSerializer < ActiveModel::Serializer
+    attributes :id, :price, :created_at
+
+    belongs_to :user, key: :bidder
+  end
+end
